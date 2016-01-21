45da93b6ffdb3ae5358073481350ea9326211f36
Rinat Mukhatrov <rin-nas@ya.ru>
2016-01-21 17:32:56 +0300

    Update jquery.pseudo_selectors2classes.js

--- jquery.pseudo_selectors2classes.js
+++ jquery.pseudo_selectors2classes.js
@@ -1,26 +1,5 @@
 /**
- * В CSS 2.1 отсутствуют псевдо-классы :focus, :checked, :read-only, :disabled, которые есть в CSS3.
- * Браузеры IE до 9 версии не поддерживают эти псевдоклассы.
- * JS код ниже по событиям браузера для элементов ввода добавляет/удаляет соответствующие обычные классы
- * в текущий и родительский элемент. Появляется возможность достичь лучшего интерактивного дизайна страниц
- * (включая старые браузеры), написав CSS правила, использующие эти классы.
- * Можно делать зависимость родительского элемента от текущего.
  *
- * HTML 		CSS3			Replacement		Replacement			Replacement
- * attribute	pseudo class	class in		class in			class in
- * 								current element parent element		parent element example
- * -------------------------------------------------------------------------------------
- * -			:hover			.hover			has-*-hover			has-radio-hover
- * -			:focus			.focus			has-*-focus			has-email-focus
- * checked		:checked		.checked		has-*-checked		has-checkbox-checked
- * selected		-				.selected		has-*-selected		has-option-selected
- * readonly		:read-only		.read-only		has-*-read-only		has-text-readonly
- * disabled		:disabled		.disabled		has-*-disabled		has-submit-disabled
- * -			:enabled		.enabled		has-*-enabled		has-select-enabled
- *
- * * -- это тип тега (атрибут type) или название самого тега, если типа нет.
- *
- * http://jsfiddle.net/qp8eoaxq/14/
  * @copyright Rinat Mukhtarov <rin-nas@ya.ru>
  * @version 2.5
  */
